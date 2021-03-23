3c75c881c2fd5d4fc382f5dd10b06d0f060614d5
Rogerhz <rogerhernandez0260@gmail.com>
2021-03-23 13:29:23 -0400

    Conditional-profile-card-generator funcional

--- src/js/index.js
+++ src/js/index.js
@@ -40,9 +40,7 @@ function render(variables = {}) {
           <h3>${variables.city ? variables.city : ``} ${
     variables.country ? variables.country : ``
   }</h3>
-          <ul class="${
-            variables.socialMediaPosition ? "position-left" : "position-right"
-          }">
+          <ul class="${variables.socialMediaPosition}">
             // <li><a href="https://twitter.com/${
               variables.twitter
             }"><i class="fa fa-twitter"></i></a></li>
